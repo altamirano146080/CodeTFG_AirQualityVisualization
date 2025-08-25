b38261b52c6e83812b3fd18dfde00f5c314ffa51
altamirano.146080 <altamirano.146080@e.unavarra.es>
2025-08-25 20:03:22 +0200

    añadiendo leyenda y dias en el slider

--- scriptPython.py
+++ scriptPython.py
@@ -1,25 +1,19 @@
 import requests
 import xml.etree.ElementTree as ET
 
-# URL del GetCapabilities de CAMS
 url = "https://eccharts.ecmwf.int/wms/?token=public&request=GetCapabilities&version=1.3.0"
 
-# Obtener el XML
 response = requests.get(url)
 xml_content = response.content
 
-# Parsear el XML
 root = ET.fromstring(xml_content)
 
-# Namespace (puede variar)
 ns = {'wms': 'http://www.opengis.net/wms'}
 
-# Buscar todas las capas
 layers = root.findall('.//wms:Layer/wms:Layer', ns)
 
 print(f"Found {len(layers)} layers:\n")
 
-# Extraer información por capa
 for layer in layers:
     name = layer.find('wms:Name', ns)
     title = layer.find('wms:Title', ns)
